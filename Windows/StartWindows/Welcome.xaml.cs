2ab25990efc39dba340d35d814380d844467fe5e
VladislavSimonin <vladislavsimonin@gmail.com>
2026-04-29 12:56:35 +0300

    Начало создания окна авторизации

---
+++ Windows/StartWindows/Welcome.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EleonHotel.Windows.StartWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Window
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+            this.Width = SystemParameters.PrimaryScreenWidth * 0.5; // 80% ширины
+            this.Height = SystemParameters.PrimaryScreenHeight * 0.5; // 80% высоты
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+    }
+}
