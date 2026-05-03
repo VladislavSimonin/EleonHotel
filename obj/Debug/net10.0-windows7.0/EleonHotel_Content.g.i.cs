557ae067d27125c91523d4191619879e3394b173
VladislavSimonin <vladislavsimonin@gmail.com>
2026-05-03 14:48:57 +0300

    Улучшено оформление окон

--- obj/Debug/net10.0-windows7.0/EleonHotel_Content.g.i.cs
+++ obj/Debug/net10.0-windows7.0/EleonHotel_Content.g.i.cs
@@ -26,7 +26,6 @@
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("roboto-thinitalic.ttf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("robotocondensed-bold.ttf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("robotocondensed-bolditalic.ttf")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("robotocondensed-italic.ttf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("robotocondensed-light.ttf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("robotocondensed-lightitalic.ttf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("robotocondensed-regular.ttf")]
