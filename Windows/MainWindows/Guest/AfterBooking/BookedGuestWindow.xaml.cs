823dae30bb13dafb155e0230a882592ccc679008
VladislavSimonin <vladislavsimonin@gmail.com>
2026-05-07 16:05:36 +0300

    2

---
+++ Windows/MainWindows/Guest/AfterBooking/BookedGuestWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EleonHotel.Windows.MainWindows.Guest
+{
+    /// <summary>
+    /// Логика взаимодействия для BookedGuestWindow.xaml
+    /// </summary>
+    public partial class BookedGuestWindow : Window
+    {
+        public BookedGuestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutMe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void orderDish_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void orderLaundry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void orderCleaning_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void orderTransfer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void inviteDoctor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
