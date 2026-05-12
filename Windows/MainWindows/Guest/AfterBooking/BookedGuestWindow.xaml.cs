8b14c81a97bdaf234966c2bc3a6b7c1bbece2797
VladislavSimonin <vladislavsimonin@gmail.com>
2026-05-12 16:40:12 +0300

    Revert "**Add payment windows for guest services with database integration**"
    This reverts commit 71a1c5c3d2a387a0c36a3a3ca2f76b8e934a0501.

--- Windows/MainWindows/Guest/AfterBooking/BookedGuestWindow.xaml.cs
+++ Windows/MainWindows/Guest/AfterBooking/BookedGuestWindow.xaml.cs
@@ -2,7 +2,6 @@
 using EleonHotel.Windows.MainWindows.Guest.AfterBooking.Payments;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,7 +20,6 @@ namespace EleonHotel.Windows.MainWindows.Guest
     public partial class BookedGuestWindow : Window
     {
         private readonly int _userId;
-        private string ConnectionString = "Server=DESKTOP-SGSC2AR\\SQLEXPRESS;Database=EleonHotel;User Id=Vladislav;Password=lolihanter1000-7;TrustServerCertificate=true;";
 
         public BookedGuestWindow(int userId)
         {
@@ -43,63 +41,23 @@ namespace EleonHotel.Windows.MainWindows.Guest
 
         private void orderLaundry_Click(object sender, RoutedEventArgs e)
         {
-            OpenServicePaymentWindow(1, "Прачечная");
+            //var laundryWindow = new LaundryPayment(_userId);
+            //laundryWindow.ShowDialog();
         }
 
         private void orderCleaning_Click(object sender, RoutedEventArgs e)
         {
-            OpenServicePaymentWindow(2, "Уборка");
+
         }
 
         private void orderTransfer_Click(object sender, RoutedEventArgs e)
         {
-            OpenServicePaymentWindow(3, "Трансфер");
+
         }
 
         private void inviteDoctor_Click(object sender, RoutedEventArgs e)
         {
-            OpenServicePaymentWindow(5, "Вызов врача");
-        }
 
-        private void OpenServicePaymentWindow(int serviceId, string serviceName)
-        {
-            try
-            {
-                decimal serviceCost = 0;
-                using (var conn = new SqlConnection(ConnectionString))
-                {
-                    conn.Open();
-                    string query = "SELECT cost FROM Additional_services WHERE service_id = @serviceId";
-                    using (var cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@serviceId", serviceId);
-                        object result = cmd.ExecuteScalar();
-                        if (result != null && result != DBNull.Value)
-                        {
-                            serviceCost = (decimal)result;
-                        }
-                    }
-                }
-
-                Window paymentWindow = serviceId switch
-                {
-                    1 => new LaundryPayment(_userId, serviceId, serviceName, serviceCost),
-                    2 => new CleaningPayment(_userId, serviceId, serviceName, serviceCost),
-                    3 => new TransferPayment(_userId, serviceId, serviceName, serviceCost),
-                    5 => new DoctorPayment(_userId, serviceId, serviceName, serviceCost),
-                    _ => null
-                };
-
-                if (paymentWindow != null)
-                {
-                    paymentWindow.Owner = this;
-                    paymentWindow.ShowDialog();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка при получении информации об услуге: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
         }
     }
 }
